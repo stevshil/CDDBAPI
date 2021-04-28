d70d2e23239b472c8715c9e2f82ee8badb800db1
Steve Shilling <stevshil@users.noreply.github.com>
2021-04-28 15:44:32 +0100

    Jenkins pipeline

--- build_container.groovy
+++ build_container.groovy
@@ -1,3 +1,30 @@
-folder ('Build_Containers') {
-    displayName('Build Containers')
+stages {
+  stage('Build DB Image') {
+    when {
+      anyOf{
+        changeset "Build_Containers/Dockerfile-mysql"
+        triggeredBy cause: "UserIdCause"
+      }
+    }
+    steps {
+      container('kaniko') {
+        sh '''echo '{ "credsStore": "ecr-login" }' > /kaniko/.docker/config.json
+        /kaniko/executor -f `pwd`/Build_Containers/Dockerfile-mysql -c `pwd` --insecure --skip-tls-verify --cache=false --destination=${ECR_REPO}:cddb-latest'''
+      }
+    }
+  }
+  stage('Build App Image') {
+    when {
+      anyOf{
+        changeset "Build_Containers/Dockerfile-cdapi"
+        triggeredBy cause: "UserIdCause"
+      }
+    }
+    steps {
+      container('kaniko') {
+        sh '''echo '{ "credsStore": "ecr-login" }' > /kaniko/.docker/config.json
+        /kaniko/executor -f `pwd`/Build_Containers/Dockerfile-cdapi -c `pwd` --insecure --skip-tls-verify --cache=false --destination=${ECR_REPO}:cdapi-latest'''
+      }
+    }
+  }
 }
